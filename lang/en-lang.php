42e6bd0da4d8f43611911016c6b6a7f7272917c8
Mazrog <maxime.cabanal@outlook.fr>
2016-12-05 18:02:40 +0100

    Correction inscription + Ajout traduction

---
+++ lang/en-lang.php
@@ -0,0 +1,48 @@
+<?php
+
+define("LANG", "en");
+
+/* Page d'accueil -- index.php */
+
+define("INPUT_USERNAME_DESC", "Username");
+define("INPUT_PASSWORD_DESC", "Password");
+
+define("CHECKBOX_TEXT", "Remember me");
+define("SIGN_IN_TEXT", "Sign in");
+define("CREATE_ACC_TEXT", "Create my account :D");
+
+define("ACCOUNT_CREATION", "Account creation");
+define("SIGN_UP_TEXT", "Sign up");
+
+
+define("ACCOUNT_CREATION_SUCCESS", "Your account has been created.");
+
+/* Errors */
+
+define("ERR_WRONG_LOGIN", "<strong>Error</strong>, wrong username/password.");
+define("ERR_USERNAME_TAKEN", "<strong>Error</strong>, username already taken.");
+define("ERR_UNKNOWN", "<strong>Unknown Error</strong><br/>Please try again later.");
+
+
+/*  Chat.php */
+define("CHAT_PLACEHOLDER", "Type your message here");
+define("SEND_BUTTON", "Send");
+
+define("MENU_SETTINGS", "Settings");
+define("MENU_LOG_OUT", "Log Out");
+
+define("MENU_CHANNELS", "Channels");
+define("CHANNEL_RESTRICTED", "Restricted");
+
+define("HELP_EMOTES", "Emotes");
+define("HELP_COMMANDS", "Chat Commands");
+define("HELP_SHORTCUTS", "Shortcuts");
+
+define("CHAN_SETTINGS_ADD", "Add");
+define("CHAN_SETTINGS_KICK", "Kick");
+define("CHAN_SETTINGS_RN", "Rename Channel");
+define("CHAN_SETTINGS_CHANGE_DESC", "Change description");
+define("CHAN_SETTINGS_QUIT", "Leave");
+
+
+?>
---
+++ lang/fr-lang.php
@@ -0,0 +1,47 @@
+<?php
+
+define("LANG", "fr");
+
+/* Page d'accueil -- index.php */
+
+define("INPUT_USERNAME_DESC", "Nom d'utilisateur");
+define("INPUT_PASSWORD_DESC", "Mot de passe");
+
+define("CHECKBOX_TEXT", "Rester actif");
+define("SIGN_IN_TEXT", "Se connecter");
+define("CREATE_ACC_TEXT", "Créer un compte");
+
+define("ACCOUNT_CREATION", "Création de compte");
+define("SIGN_UP_TEXT", "S'enregistrer");
+
+
+define("ACCOUNT_CREATION_SUCCESS", "Votre compte a bien été créé.");
+
+/* Errors */
+
+define("ERR_WRONG_LOGIN", "<strong>Erreur</strong> Mauvais nom d'utilisateur/mot de passe.");
+define("ERR_USERNAME_TAKEN", "<strong>Erreur</strong> Nom d'utilisateur déjà utilisé.");
+define("ERR_UNKNOWN", "<strong>Erreur inconnue</strong> <br/> Veuillez réessayer plus tard.");
+
+
+/*  Chat.php */
+define("CHAT_PLACEHOLDER", "Entrez votre message ici");
+define("SEND_BUTTON", "Envoyer");
+
+define("MENU_SETTINGS", "Options");
+define("MENU_LOG_OUT", "Se déconnecter");
+
+define("MENU_CHANNELS", "Canaux");
+define("CHANNEL_RESTRICTED", "Privé");
+
+define("HELP_EMOTES", "Emoticônes");
+define("HELP_COMMANDS", "Commandes du chat");
+define("HELP_SHORTCUTS", "Raccourcis");
+
+define("CHAN_SETTINGS_ADD", "Ajouter");
+define("CHAN_SETTINGS_KICK", "Exclure");
+define("CHAN_SETTINGS_RN", "Renommer canal");
+define("CHAN_SETTINGS_CHANGE_DESC", "Changer description");
+define("CHAN_SETTINGS_QUIT", "Quitter");
+
+?>
